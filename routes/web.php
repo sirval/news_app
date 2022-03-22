59fdc727d982e6c25e749475a9fea6222301ca54
sirval <ohukaiv@gmail.com>
2022-03-22 12:14:47 +0100

    imported some classes

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\LoginController;
+use App\Http\Controllers\LogoutController;
 use App\Http\Controllers\NewsPostController;
 use App\Http\Controllers\RegisterController;
 
