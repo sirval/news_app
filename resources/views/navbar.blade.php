870aab1f0fc6755e5c92c71af96eb89343624740
sirval <ohukaiv@gmail.com>
2022-03-31 04:50:44 +0100

    initial commit

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -4,9 +4,7 @@
     <div class="container d-flex align-items-center">
 
       <h1 class="logo me-auto"><a href="../">News Hub</a></h1>
-      <!-- Uncomment below if you prefer to use an image logo -->
-      <!-- <a href="index.html" class="logo me-auto"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
-
+     
       <nav id="navbar" class="navbar order-last order-lg-0">
         <ul>
           <li><a class="nav-link scrollto active" href="../">Home</a></li>
